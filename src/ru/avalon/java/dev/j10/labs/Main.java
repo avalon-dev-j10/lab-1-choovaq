0232207e7c7014050e980972920177ea1a7460df
choovaq <abrsnv@gmail.com>
2019-03-26 01:08:48 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,17 +1,32 @@
 package ru.avalon.java.dev.j10.labs;
 
 import ru.avalon.java.dev.j10.labs.models.Person;
+import ru.avalon.java.dev.j10.labs.models.Passport;
+import ru.avalon.java.dev.j10.labs.commons.Address;
 
 public class Main {
 
-    Main() {
-
+    public static void main(String args[]) {        
         /*
          * FIXME(Студент): Измените определение метода так, чтобы он стал точкой входа в приложение.
          */
-
-        Person ivanov = null;
-        Person smith = null;
+        
+        Passport ivanovPassport = new Passport(1425, "Иванов", "Иван", "Иванович", null, "01.01.2001", "02.02.2015", "ТП№1");
+        Address ivanovAddress = new Address("Saint-Petersburg", "Nevsky", 12);
+        Person ivanov = new Person("Ivanov", ivanovPassport, ivanovAddress);
+        
+        Passport smithPassport = new Passport(5241, "John", "Smith", null, "Edvard", "12.12.2012", null, null);
+        Address smithAddress = new Address("Helsinki", "Central", 21);
+        Person smith = new Person("Smith",smithPassport, smithAddress);
+        
+        String nameIvanov = ivanov.getFullName();
+        System.out.println(nameIvanov);
+        String nameSmith = smith.getFullName();
+        System.out.println(nameSmith);
+        String addressIvanov = ivanov.getAddress();
+        System.out.println(addressIvanov);
+        String addressSmith = smith.getAddress();
+        System.out.println(addressSmith);
 
         /*
          * TODO(Студент): Создайте экземпляры класса 'Person'
