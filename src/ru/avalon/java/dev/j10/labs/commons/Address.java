810ec7168ef14dddb82d8fa31494c8d2a02283e8
choovaq <abrsnv@gmail.com>
2019-03-26 01:09:34 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/commons/Address.java
+++ src/ru/avalon/java/dev/j10/labs/commons/Address.java
@@ -1,3 +1,42 @@
+package ru.avalon.java.dev.j10.labs.commons;
+
+public class Address{
+    String city;
+    String street;
+    int house;
+    
+    public Address(String city, String street, int house){
+        this.city = city;
+        this.street = street;
+        this.house = house;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+    
+}
+
 /*
  * TODO(Студент): Создайте класс Address.
  *
